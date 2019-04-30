1d8626fe12175a5252179fc73581ce5b2a5c1192
aaronsato <2asato@gmail.com>
2019-04-30 14:04:25 -0700

    header in place

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 console.log('hi');
+
+$(function () {
+    $(document).scroll(function () {
+      var $nav = $("#mainNavbar");
+      $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+    });
+  });
