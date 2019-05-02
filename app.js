edf201089faf4748b1aa7a2295c4a19c3e46f920
aaronsato <2asato@gmail.com>
2019-05-02 10:06:28 -0700

    added jquery page scroll animation

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 console.log('hi');
 
+$(".navbar a").click(function () {
+    $("body,html").animate({
+        scrollTop: $("#" + $(this).data('value')).offset().top
+    }, 1000)
+
+})
+
 $(function () {
     $(document).scroll(function () {
-      var $nav = $("#mainNavbar");
-      $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+        var $nav = $("#mainNavbar");
+        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
     });
-  });
+});
+
+
