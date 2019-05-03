48b5a1e0703838df808b5cc91cd05987387d7ad0
aaronsato <2asato@gmail.com>
2019-05-02 18:22:26 -0700

    added collier video i think its all set

--- app.js
+++ app.js
@@ -16,4 +16,11 @@ $(function () {
     });
 });
 
+$(document).ready(function() {
+    $('#collierVidLink').magnificPopup({
+      type:'inline',
+      midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+    });         
+  });
+
 
