f2a8269f5bfd8a3024a58f51c710a8294c372c5a
aaronsato <2asato@gmail.com>
2019-05-03 21:48:27 -0700

    blair video in place but need new video

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ $(function () {
 });
 
 $(document).ready(function() {
-    $('#collierVidLink').magnificPopup({
+    $('.vidLink').magnificPopup({
       type:'inline',
       midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
     });         
